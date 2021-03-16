0c8e955825d3f19f08723b36f7cb3dd386f8f863
vRedoak <krasnodubskaya.v@gmail.com>
2021-03-16 21:06:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Здраствуйте! Для создания треугольника вам необходимо ввести три его стороны.");
+                Console.Write("A = ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("B = ");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("C = ");
+                double c = double.Parse(Console.ReadLine());
+                Triangle triangle = new Triangle(a, b, c);
+                Console.WriteLine("Площадь треугольник: "+ triangle.Square());
+                Console.WriteLine("Периметр треугольника: " + triangle.Perimeter());
+            }
+            catch (Exception e) { Console.WriteLine("Ошибка: " + e.Message); }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Triangle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    public class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+            if (!this.TriangleExistenceTest()) throw new Exception("Сообщение конструктора : Такого треугольника не может быть");
+        }
+
+        public double A
+        {
+            get
+            { return a; }
+           private set
+            {
+                if (value < 0) throw new Exception("Значение не может быть отрицательным");
+                a = value;
+                
+            }
+        }
+        public double B
+        {
+            get
+            { return b; }
+            private set
+            {
+                if (value < 0) throw new Exception("Значение не может быть отрицательным");
+                b = value;
+            }
+        }
+        public double C
+        {
+            get
+            {  return c; }
+            private set
+            {
+                if (value < 0) throw new Exception("Значение не может быть отрицательным");
+                c = value;
+            }
+        }
+
+        public bool TriangleExistenceTest() => (a + b > c && a + c > b && c + b > a);
+       
+        public double Square()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Round(Math.Sqrt(p * (p - a) * (p - b) * (p - c)), 2);
+        }
+
+        public double Perimeter() => a + b + c;
+    }
+}
